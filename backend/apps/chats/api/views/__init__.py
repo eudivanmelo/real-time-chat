a318d2554b1863b9448a934e21f76bc6217e7f51
eudivan44 <eudivan44@gmail.com>
2024-08-12 16:14:29 +0000

    chatmessage api finished

--- backend/apps/chats/api/views/__init__.py
+++ backend/apps/chats/api/views/__init__.py
@@ -1,6 +1,9 @@
 from .chats import ChatView, ChatsView
+from .messages import ChatMessageView, ChatMessagesView
 
 __all__ = [
     'ChatsView',
-    'ChatView'
+    'ChatView',
+    'ChatMessagesView',
+    'ChatMessageView'
 ]
